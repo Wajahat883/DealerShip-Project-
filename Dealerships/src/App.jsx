d7e9841bbca3a5e55eba80a05e977fb3570e0df7
Wajahat883 <waji21569870@gmail.com>
2025-08-18 13:56:40 +0500

    add folders

---
+++ Dealerships/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Welcome to the Dealerships App!
+      </h1>
+    </div>
+  );
+}
+export default App;
